4741b451a9982987d406d23a964f93b59b65c914
T <tzoko123@outlook.com>
2020-12-13 13:51:34 -0500

    Initial commit

--- 04_load/load_from_local.sh
+++ 04_load/load_from_local.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-LOC="--location US"
-
-bq $LOC mk ch03
-
-zless ./college_scorecard.csv.gz | sed 's/PrivacySuppressed/NULL/g' | gzip > /tmp/college_scorecard.csv.gz
-
+PROJECT_ID="learning-gcp-dev-298517"
+LOC="--location=US"
 #SCHEMA="--autodetect"
 SCHEMA="--schema=schema.json --skip_leading_rows=1"
+
+gcloud config set project $PROJECT_ID
+
+# If you set the default project_id using gcloud, no need to specify --project-id flag for bq cmd
+bq --project_id=$PROJECT_ID --location=us mk --dataset_id=ch04
+zless ./college_scorecard.csv.gz | sed 's/PrivacySuppressed/NULL/g' | gzip > /tmp/college_scorecard.csv.gz
 
 bq $LOC \
    load --null_marker=NULL --replace \
    --source_format=CSV $SCHEMA \
-   ch03.college_scorecard \
+   ch04.college_scorecard \
+    #   ./college_scorecard.csv.gz \        
+    # In the book, this is the provided snippet, but when running will encouter errors
+    # Therefore, we use this cmd:
+    # zless ./college_scorecard.csv.gz | sed 's/PrivacySuppressed/NULL/g' | gzip > /tmp/college_scorecard.csv.gz
+    # to resolve the issue, then load the file from /tmp/college_scorecard.csv.gz
    /tmp/college_scorecard.csv.gz
-
-#   ./college_scorecard.csv.gz \
